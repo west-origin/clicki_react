20d5f035606599d150885bc60c0224347476a85f
seoseowonwon <mrkiwonseo@gmail.com>
2025-12-03 23:24:31 +0900

    Main.js css, js import 수정 및 파일 추가

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 function SearchBar() {
     const [keyword, setKeyword] = useState("");
@@ -19,34 +20,44 @@ function SearchBar() {
     };
 
     return (
-        <div className="col-md-4 col-lg-6 text-center">
-            <div class="position-relative ps-4">
-            <div class="d-flex border rounded-pill">
-                <input class="form-control border-0 rounded-pill w-100 py-3" type="text" data-bs-target="#dropdownToggle123" placeholder="찾고 싶은 상품을 검색해 보세요!"
-                value={keyword} onChange={(e) => setKeyword(e.target.value)}/>
+        <>
+            <Helmet>
+                <link
+                  rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+                />
+            </Helmet>
 
-                <select class="form-select text-dark border-0 border-start rounded-0 p-3" style={{width: 200}}>
-                    <option value="All Category">전체</option>
-                    <option value="Pest Control-2">패션의류/잡화</option>
-                    <option value="Pest Control-3">뷰티</option>
-                    <option value="Pest Control-4">출산/유아동</option>
-                    <option value="Pest Control-5">식품</option>
-                    <option value="Pest Control-6">주방용품</option>
-                    <option value="Pest Control-7">생활용품</option>
-                    <option value="Pest Control-8">홈인테리어</option>
-                    <option value="Pest Control-9">가전디지털</option>
-                    <option value="Pest Control-10">스포츠/레저</option>
-                    <option value="Pest Control-5">자동차용품</option>
-                    <option value="Pest Control-5">도서/음반/DVD</option>
-                    <option value="Pest Control-5">완구/취미</option>
-                    <option value="Pest Control-5">문구/오피스</option>
-                    <option value="Pest Control-5">반려동물용품</option>
-                    <option value="Pest Control-5">헬스/건강식품</option>
-                    </select>
-                    <button type="button" onClick={handleSearch} className="btn btn-primary rounded-pill py-3 px-5" style={{border: 0}}><i className="fas fa-search"></i></button>
+            <div className="col-md-4 col-lg-6 text-center">
+                <div class="position-relative ps-4">
+                    <div class="d-flex border rounded-pill">
+                        <input class="form-control border-0 rounded-pill w-100 py-3" type="text" data-bs-target="#dropdownToggle123" placeholder="찾고 싶은 상품을 검색해 보세요!"
+                        value={keyword} onChange={(e) => setKeyword(e.target.value)}/>
+
+                        <select class="form-select text-dark border-0 border-start rounded-0 p-3" style={{width: 200}}>
+                            <option value="All Category">전체</option>
+                            <option value="Pest Control-2">패션의류/잡화</option>
+                            <option value="Pest Control-3">뷰티</option>
+                            <option value="Pest Control-4">출산/유아동</option>
+                            <option value="Pest Control-5">식품</option>
+                            <option value="Pest Control-6">주방용품</option>
+                            <option value="Pest Control-7">생활용품</option>
+                            <option value="Pest Control-8">홈인테리어</option>
+                            <option value="Pest Control-9">가전디지털</option>
+                            <option value="Pest Control-10">스포츠/레저</option>
+                            <option value="Pest Control-5">자동차용품</option>
+                            <option value="Pest Control-5">도서/음반/DVD</option>
+                            <option value="Pest Control-5">완구/취미</option>
+                            <option value="Pest Control-5">문구/오피스</option>
+                            <option value="Pest Control-5">반려동물용품</option>
+                            <option value="Pest Control-5">헬스/건강식품</option>
+                            </select>
+                        <button type="button" onClick={handleSearch} className="btn btn-primary rounded-pill py-3 px-5" style={{border: 0}}><i className="fas fa-search"></i></button>
+                    </div>
                 </div>
             </div>
-        </div>
+
+        </>
     );
 }
 export default SearchBar;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import SearchBar from "../components/SearchBar";
-
+import "../lib/owlcarousel/assets/owl.carousel.min.css";
+import "../lib/animate/animate.min.css";
+import "../lib/wow/wow.min.js";
 function Main() {
   return (
     <>
@@ -11,30 +13,6 @@ function Main() {
             <meta content="width=device-width, initial-scale=1.0" name="viewport" />
             <meta content="" name="keywords" />
             <meta content="" name="description" />
-            {/* Google Web Fonts */}
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-
-            <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
-            rel="stylesheet"
-            />
-            {/* Icon Font Stylesheet */}
-            <link
-            rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
-            />
-            <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
-            rel="stylesheet"
-            />
-            {/* Libraries Stylesheet */}
-            <link href="lib/animate/animate.min.css" rel="stylesheet" />
-            <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
-            {/* Customized Bootstrap Stylesheet */}
-            <link href="css/bootstrap.min.css" rel="stylesheet" />
-            {/* Template Stylesheet */}
-            <link href="css/style.css" rel="stylesheet" />
         </Helmet>
 
       {/* Topbar Start */}
